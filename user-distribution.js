d71584ef24bfd2c54097bfbcbcd4383c466cf2f6
Dongdong Tian <seisman.info@gmail.com>
2016-04-07 21:00:22 +0800

    First Map using Leaflet
    1. Leaflet
    2. Leaflet.Coordinates
    3. Leaflet.ChineseTmsProviders

---
+++ user-distribution.js
@@ -0,0 +1,15 @@
+var Institutions = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [117.2653, 31.8433]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "<b>合肥市</b> <br /> 中国科学技术大学 地球和空间科学学院"
+            },
+        },
+    ]
+};
